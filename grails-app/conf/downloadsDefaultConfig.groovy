c4af71d47258b2622474027ea4e80fa185956bc1
nickdos <nickdos@gmail.com>
2016-05-27 16:06:49 +1000

    legacy download  fields added to defaultConfig
    Not used yet.

--- grails-app/conf/downloadsDefaultConfig.groovy
+++ grails-app/conf/downloadsDefaultConfig.groovy
@@ -53,8 +53,8 @@ downloads.environmentalLayers = ["el.p"]
 downloads.contextualLayers = ["cl.p"]
 //biocache.downloads.extra = "dataResourceUid,dataResourceName.p,occurrenceStatus.p"
 
-
-
+// legacy default fields taken from https://github.com/AtlasOfLivingAustralia/biocache-service/blob/master/src/main/java/au/org/ala/biocache/dto/DownloadRequestParams.java#L35
+downloads.legacy.defaultFields = "uuid,catalogNumber,taxonConceptID.p,scientificName,vernacularName,scientificName.p,taxonRank.p,vernacularName.p,kingdom.p,phylum.p,classs.p,order.p,family.p,genus.p,species.p,subspecies.p,institutionCode,collectionCode,locality,decimalLatitude,decimalLongitude,geodeticDatum,decimalLatitude.p,decimalLongitude.p,coordinatePrecision,coordinateUncertaintyInMeters.p,country.p,ibra.p,imcra.p,stateProvince.p,cl959,minimumElevationInMeters,maximumElevationInMeters,minimumDepthInMeters,maximumDepthInMeters,individualCount,recordedBy,year.p,month.p,day.p,eventDate.p,eventTime.p,basisOfRecord,basisOfRecord.p,sex,preparations,informationWithheld.p,dataGeneralizations.p,speciesHabitats.p,outlierForLayers.p,taxonomicIssue.p,geospatiallyKosher"
 downloads.mandatoryFields = ["recordLevelTerms","occurrence"]
 downloads.excludeFields = ""
 downloads.dwcFields = "uuid,catalogue_number,taxon_concept_lsid,raw_taxon_name,raw_common_name,scientificName.p,raw_taxon,common_name,kingdom,phylum,class,order,family,genus,species,subspecies_name,institution_code,collection_code,raw_locality,decimalLatitude,decimalLongitude,decimalLatitude.p,decimalLongitude.p,coordinatePrecision,coordinate_uncertainty,country,cl1048,cl21,state,cl23,min_elevation_d,max_elevation_d_rng,min_depth_d,max_depth_d,individualCount,collector,year,month,day.p,eventDate.p,eventTime.p,raw_basis_of_record,basis_of_record,raw_sex,preparations,informationWithheld.p,dataGeneralizations.p"
